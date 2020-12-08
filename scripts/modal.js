e6b3b6c0742af9f339d930ada96408ecd54446ee
Artsiom <artsiom.sakratar@gmail.com>
2020-12-08 16:53:55 +0300

    fix modal and person-about

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,26 +1,74 @@
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById('myImg');
-var imgs = document.querySelectorAll('.img-modal');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-
-for (const img of imgs) {
-
-    img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
-
+let imgs = document.querySelectorAll('.img');
+for (let i = 0; i < imgs.length; i++) {
+  imgs[i].onclick = function () {
+    openModal();
+    currentSlide(i + 1);
+  }
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let modal = document.querySelector('.modal');
+modal.onclick = function(e) {
+  let el = e ? e.target : window.EventSource;
+  if (el.className === 'modal') closeModal();
+}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
+let closeCursor = document.querySelector('.close.cursor');
+closeCursor.onclick = function () {
+  closeModal();
+}
+
+let prev = document.querySelector('.prev');
+prev.onclick = function () {
+  plusSlides(-1);
+}
+
+let next = document.querySelector('.next');
+next.onclick = function () {
+  plusSlides(1);
+}
+
+let columns = document.querySelectorAll('.column img');
+for (let i = 0; i < columns.length; i++) {
+  columns[i].onclick = function () {
+    currentSlide(i + 1);
+  }
+}
+
+
+function openModal() {
+  document.querySelector('.modal').style.display = "block";
+}
+
+function closeModal() {
+  document.querySelector('.modal').style.display = "none";
+}
+
+
+let slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
--- scripts/team_person.js
+++ scripts/team_person.js
@@ -1,16 +1,43 @@
 let teamPerson = document.querySelector('.team-person');
 
+let team = [
+    {
+        name: "Dianne Hickman",
+        position: "UI Designer",
+        description: "Designers also research targeted users to develop a clear understanding of their needs."
+    }, {
+        name: "Woody Rogers",
+        position: "QA Automation",
+        description: "Refers to using a separate piece of software to run tests on the software that you're developing."
+    },{
+        name: "Verity Morgan",
+        position: "Developer",
+        description: "Engineer who can handle all the work of databases, servers, systems engineering, and clients."
+    }, {
+        name: "Korey Bernard",
+        position: "Project Manager",
+        description: "Process of leading the work of a team to achieve goals and meet success criteria at a specified time."
+    },{
+        name: "Kathryn Christian",
+        position: "Business Analyst",
+        description: "Used to identify and articulate the need for change in how organizations work."
+    }, {
+        name: "Elisa Gale",
+        position: "Team Lead",
+        description: "Provides guidance, instruction, direction and leadership to a group of individuals."
+    }
+];
+
 const TEAM_SIZE = 6;
 
-for (let i = 1; i <= TEAM_SIZE; i++) {
-    
+for (let i = 0; i < TEAM_SIZE; i++) {
     let person = document.createElement('div');
     person.classList.add('person');
 
-    let img = getImg(i);
+    let img = getImg(i+1);
     person.append(img);
 
-    let personAbout = createPersonAbout();
+    let personAbout = createPersonAbout(i);
     person.append(personAbout);
 
     teamPerson.append(person);
@@ -19,118 +46,82 @@ for (let i = 1; i <= TEAM_SIZE; i++) {
 /*  #####  FUNCTIONS  #####  */
 
 function getImg(personNum) {
-    
     let img = document.createElement('img');
     img.src = `./images/team_${personNum}.png`;
     img.alt = `Person ${personNum}`;
-
     return img;
 }
 
-function createPersonAbout() {
-
+function createPersonAbout(personNum) {
     let personAbout = document.createElement('div');
     personAbout.classList.add('person-about');
-
-    let personName = createPersonName();
+    let personName = createPersonName(personNum);
     personAbout.append(personName);
-
-    let description = createDescription();
+    let description = createDescription(personNum);
     personAbout.append(description);
-
     let personSocial = createPersonSocial()
     personAbout.append(personSocial);
-
     return personAbout;
 }
 
-function createPersonName() {
-    
+function createPersonName(personNum) {
     let personName = document.createElement('div');
     personName.classList.add('person-name');
-
-    personName.innerHTML = 'AL RAYHAN <span>/ UI Designer</span>';
-
+    personName.innerHTML = `${team[personNum].name} <span>/ ${team[personNum].position}</span>`;
     return personName;
 }
 
-function createDescription() {
-    
+function createDescription(personNum) {
+    let descriptionContainer = document.createElement('div');
+    descriptionContainer.classList.add('description');
     let description = document.createElement('p');
-
-    description.innerHTML =
-        `Lorem Ipsum is not simply is an<br />
-        action designer random text<br />
-        It has roots in a piece.`;
-    
-    return description;
+    description.innerHTML = team[personNum].description;
+    descriptionContainer.append(description);
+    return descriptionContainer;
 }
 
 function createPersonSocial() {
-    
     let personSocial = document.createElement('div');
     personSocial.classList.add('person-social');
-
-
     let facebook = getSocial('facebook');
     personSocial.append(facebook);
-
     let twitter = getSocial('twitter');
     personSocial.append(twitter);
-
     let dribbble = getSocial('dribbble');
     personSocial.append(dribbble);
-
     let mail = getSocial('mail');
     personSocial.append(mail);
-
-
     return personSocial;
 }
 
 function getSocial(service) {
-
     let social = document.createElement('a');
     social.target = '_blank'
-
     let socialIcon = document.createElement('i');
 
     switch (service) {
-
         case 'facebook':
-            
             social.href = 'https://www.facebook.com/';
             socialIcon.classList.add('fab');
             socialIcon.classList.add('fa-facebook-f');
-
             break;
-        
         case 'twitter':
-
             social.href = 'https://twitter.com/';
             socialIcon.classList.add('fab');
             socialIcon.classList.add('fa-twitter');
-            
             break;
-        
         case 'dribbble':
-
             social.href = 'https://dribbble.com/';
             socialIcon.classList.add('fab');
             socialIcon.classList.add('fa-dribbble');
-            
             break;
-        
         case 'mail':
-
             social.href = 'https://mail.google.com/';
             socialIcon.classList.add('far');
             socialIcon.classList.add('fa-envelope');
-            
             break;
     }
 
     social.append(socialIcon);
-    
     return social;
 }
